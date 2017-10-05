e33f306a779340d56408cc20f6a524f15f53b7eb
VanJoel <alexjodom@gmail.com>
2017-10-05 14:45:55 -0400

    Added support for Jetson comucation

---
+++ src/org/usfirst/frc/team4910/subsystems/Jetson.java
@@ -0,0 +1,61 @@
+package org.usfirst.frc.team4910.subsystems;
+
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+
+import com.google.protobuf.*;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import org.usfirst.frc.team4910.util.Peg;
+
+/**
+ * This is the class that comuicates with the jetson
+ */
+public class Jetson extends Subsystem 
+{
+	// This stores the context of the system
+	private Context context;
+	
+	// The socket for commuicating with the jetson
+	private Socket sub;
+
+	/**
+	 * This command runs on robot init
+	 * It sets up the context and socket and connects to the jetson PUB 
+	 */
+	public void initDefaultCommand() {
+		context = ZMQ.context(1);
+		sub = context.socket(ZMQ.SUB);
+		
+		sub.setConflate(true); // Only get the most recent messages
+		sub.connect("tcp://10.49.10.200:49100");
+		sub.subscribe("".getBytes());
+	}
+
+	public Peg.PegLoc receave() {
+		Peg.PegLoc loc;
+		byte[] message;
+
+		System.out.println("Starting recv");
+		message = sub.recv();
+		try {
+			loc = Peg.PegLoc.parseFrom(message);
+		} catch (Exception e) {
+			System.out.println("Protocall Buffer Error");
+			e.printStackTrace();
+			return null;
+		}
+		
+		return loc;
+	}
+	
+	/**
+	 * Cleans up ZMQ to prevent memory attacks
+	 * NOTE: This fuction MUST BE CALLED in the robot deinitalsa procces
+	 */
+	public void destroyZMQ(){
+		sub.close();
+		context.term();
+	}
